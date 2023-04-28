fb0914f812058ecbc1261614a0a02e893da1fdd7
Vi Hoàng Sơn <vihoangson@gmail.com>
2023-04-28 11:55:45 +0700

    ok

--- src/Progress/TextToSpeech.php
+++ src/Progress/TextToSpeech.php
@@ -1,5 +1,7 @@
 <?php
 namespace Sonlib\Progress ;
+use Exception;
+
 /**
  *
  */
@@ -12,9 +14,9 @@ class TextToSpeech
 
 
 	public function convert($text){
-        if($this->key) 
+        if($this->key)
             throw new Exception('Please insert key');
-        
+
         $curl = curl_init();
         $text = trim($text);
         if ($text === '') {
@@ -60,8 +62,7 @@ class TextToSpeech
         $response = curl_exec($curl);
 
         curl_close($curl);
-var_dump($response);
-die;
+
         if (isset($response)) {
             $f = base64_decode(json_decode($response)->audioContent);
             header('Content-Type: audio/mpeg');
@@ -83,45 +84,4 @@ die;
             header("HTTP/1.0 404 Not Found");
         }
 	}
-
-    public function originConvert($text){
-
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://content-texttospeech.googleapis.com/v1beta1/text:synthesize?alt=json&key=AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS =>'{"audioConfig":{"audioEncoding":"MP3","speakingRate":1,"volumeGainDb":0},"input":{"text":"1111"},"voice":{"languageCode":"vi-VN","name":"vi-VN-Wavenet-D"}}',
-            CURLOPT_HTTPHEADER => array(
-                'authority:  content-texttospeech.googleapis.com',
-                'x-goog-encode-response-if-executable:  base64',
-                'x-origin:  https://explorer.apis.google.com',
-                'x-clientdetails:  appVersion=5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F85.0.4183.102%20Safari%2F537.36&platform=Win32&userAgent=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20KHTML%2C%20like%20Gecko)%20Chrome%2F85.0.4183.102%20Safari%2F537.36',
-                'user-agent:  Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
-                'content-type:  application/json',
-                'x-requested-with:  XMLHttpRequest',
-                'x-javascript-user-agent:  apix/3.0.0 google-api-javascript-client/1.1.0',
-                'x-referer:  https://explorer.apis.google.com',
-                'accept:  */*',
-                'origin:  https://content-texttospeech.googleapis.com',
-                'x-client-data:  CI22yQEIorbJAQjBtskBCKmdygEImbXKAQj1x8oBCOfIygEI6cjKAQisycoBCLTLygEY6r/KAQ==',
-                'sec-fetch-site:  same-origin',
-                'sec-fetch-mode:  cors',
-                'sec-fetch-dest:  empty',
-                'referer:  https://content-texttospeech.googleapis.com/static/proxy.html?usegapi=1&jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.vi.GvGQknA8E1g.O%2Fam%3DwQE%2Fd%3D1%2Ft%3Dzgms%2Frs%3DAGLTcCMsrxwKQn_wxHYgIxdxdRd5qnmjng%2Fm%3D__features__',
-                'accept-language:  vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5'
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        echo $response;
-    }
 }
